c96103f3ebd2e0ea19c2f1897864594779f9bac9
rkasale28 <rohitkasale28@gmail.com>
2020-06-02 17:52:23 +0530

    Added Images in Database

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from flask import Flask,request,redirect,render_template
+from flask import Flask,request,redirect,render_template,send_from_directory
 from models import db,Book,migrate
 from flask_migrate import MigrateCommand
 from flask_script import Manager
+from flask_uploads import configure_uploads, IMAGES, UploadSet
 
 def create_app():
     app=Flask(__name__)
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@127.0.0.1:5432/books'
+    app.config['UPLOADED_IMAGES_DEST']='media/images'
     db.init_app(app)
     migrate.init_app(app, db)
     return app
@@ -15,12 +17,17 @@ app=create_app()
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
+images=UploadSet('images',IMAGES)
+configure_uploads(app,images)
+
 @app.route('/',methods=['POST','GET'])
 def index():
     if request.method=='POST':
         book_name=request.form['bookname']
         book_price=request.form['price']
-        newbook=Book(name=book_name,price=book_price)
+        cover_page=request.files['cover_page']
+        filename=images.save(cover_page)
+        newbook=Book(name=book_name,price=book_price,filename=filename)
 
         try:
             db.session.add(newbook)
@@ -29,7 +36,7 @@ def index():
         except:
             return 'There was an issue adding book'
     else:
-        books=Book.query.all()
+        books=Book.query.order_by('name').all()
         return render_template('index.html', books=books)
     
 @app.route('/delete/<int:id>')
@@ -49,6 +56,9 @@ def update(id):
     if request.method=='POST':
         book.name=request.form['name']
         book.price=request.form['price']
+        cover_page=request.files['cover_page']
+        book.filename=images.save(cover_page)
+        
         try:
             db.session.commit()
             return redirect('/')
@@ -56,6 +66,10 @@ def update(id):
             return 'There was an issue updating your book'
     else:
         return render_template('update.html',book=book)
+
+@app.route('/media/images/<filename>')
+def retrieve_image(filename):
+    return send_from_directory(app.config['UPLOADED_IMAGES_DEST'],filename)
 
 if __name__=="__main__":
     manager.run()
--- models.py
+++ models.py
@@ -8,6 +8,7 @@ class Book(db.Model):
     id=db.Column(db.Integer,primary_key=True)
     name=db.Column(db.String(100),nullable=False)
     price=db.Column(db.Integer)
+    filename=db.Column(db.String(100))
     
     def __repr__(self):
         return '<Book %r>' % self.id
